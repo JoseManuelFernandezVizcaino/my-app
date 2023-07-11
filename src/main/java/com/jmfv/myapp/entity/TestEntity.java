cc335459ebb3882cb10ef974bcae813b11ccb102
JoseManuelFernandezVizcaino <jmfernandezv03@gmail.com>
2023-07-11 22:52:21 +0200

    Nombre del proyecto: my-app Descripcion: Version 1

---
+++ src/main/java/com/jmfv/myapp/entity/TestEntity.java
@@ -0,0 +1,30 @@
+/**
+ * 
+ */
+package com.jmfv.myapp.entity;
+
+/**
+ * @author jmfer
+ *
+ */
+public class TestEntity {
+	
+	/**
+	 * Propiedad de prueba para la muestra de compilación del archivo testentity
+	 */
+	private String nombre;
+
+	/**
+	 * @return the nombre
+	 */
+	public String getNombre() {
+		return nombre;
+	}
+
+	/**
+	 * @param nombre the nombre to set
+	 */
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+}
